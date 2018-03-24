4b96ac93730c22aade8d64f459b4ec529155158b
Josep Carner <josepcarner@MBP-del-Josep.fios-router.home>
2018-03-23 20:07:02 -0400

    push

--- dataloaders/single_file_str_words.py
+++ dataloaders/single_file_str_words.py
@@ -1,6 +1,7 @@
 import torch, unidecode, random, os
 from torch.autograd import Variable
 from torch.utils.data import Dataset
+from collections import Counter
 
 from utils import ALL_CHARS, is_remote
 
@@ -20,6 +21,7 @@ class MyDataset(Dataset):
         self.words = [word.lower() for word in self.words.split(' ') if word.strip() != '']
 
         self.create_word_dict()
+        self.create_word_count()
         self.len = len(self.words)
 
     def create_word_dict(self):
@@ -27,6 +29,12 @@ class MyDataset(Dataset):
         if not os.path.isfile(word_dict_file):
             word_dict = {w: i for i, w in enumerate(set(self.words))}
             torch.save(word_dict, word_dict_file)
+
+    def create_word_count(self):
+        word_count_file = self.opt.input_file_train + '.word_count'
+        if not os.path.isfile(word_count_file):
+            word_count = Counter(self.words)
+            torch.save(word_count, word_count_file)
 
     def __getitem__(self, index):
         random.seed(index)
