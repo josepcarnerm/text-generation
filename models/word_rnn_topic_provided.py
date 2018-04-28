b2ec103c2600ac846991e3b9069b5e8b2065e93c
Josep Carner <josepcarner@MBP-del-Josep.fios-router.home>
2018-04-27 22:51:46 -0400

    push

--- models/word_rnn_topic_provided.py
+++ models/word_rnn_topic_provided.py
@@ -36,12 +36,13 @@ class Model(WordRNNModel):
     def initialize(self, baseline_model):
         # baseline_model must be path to "checkpoint" file
         baseline = torch.load(baseline_model).get('model')
-        self.word2idx = baseline.word2idx
         self.baseline = baseline
+        self.word2idx = baseline.word2idx
         self.encoder.load_state_dict(baseline.encoder.state_dict())
         self.rnn.load_state_dict(baseline.rnn.state_dict())
         self.decoder.load_state_dict(baseline.decoder.state_dict())
         self.encoder_topic.load_state_dict(baseline.encoder.state_dict())  # Initialize encoder_topic with encoder
+        import pdb; pdb.set_trace()
 
     def load_word_counts(self):
         if self.opt.use_pretrained_embeddings:
