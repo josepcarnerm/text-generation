216fe7f369276da0b19d952d0176c616dd8ca9d0
Josep Carner <josepcarner@MBP-del-Josep.fios-router.home>
2018-04-27 23:15:50 -0400

    push

--- models/word_rnn_topic_least_frequent_words.py
+++ models/word_rnn_topic_least_frequent_words.py
@@ -191,7 +191,8 @@ class Model(WordRNNModelTopic):
         self.losses_reconstruction.append(loss_reconstruction.data[0])
         self.losses_topic.append(loss_topic.data[0])
 
-        return self.opt.loss_alpha*loss_reconstruction + (1-self.opt.loss_alpha)*loss_topic
+        ratio = float(loss_reconstruction.detach().cpu().data.numpy()[0] / loss_topic.detach().cpu().data.numpy()[0])
+        return self.opt.loss_alpha * loss_reconstruction + (1 - self.opt.loss_alpha) * loss_topic * ratio
 
     def test(self, prime_words, predict_len, temperature=0.8):
 
